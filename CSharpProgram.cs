921f47e272aa023d5157cb623d86680b90775e72
Lacey Walker <bluelarksong@gmail.com>
2021-05-13 00:46:08 -0500

    setting up a C# project

---
+++ CSharpProgram.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Program
+{
+    static double Price(int quantity)
+    {
+        double pricePerUnit;
+        if (quantity > 100)
+        {
+            pricePerUnit = 1.5;
+        }
+    }
+}
+
+// Type "dotnet run" into the console to run the project (errors will appear, but this is step one)
