4b73193ba9fd88ee960f4e51cd0db1bfa94ea776
Lacey Walker <bluelarksong@gmail.com>
2021-05-13 01:56:00 -0500

    finished up notes on CompileErrors

--- CSharp/CompileErrors.cs
+++ CSharp/CompileErrors.cs
@@ -12,3 +12,4 @@ class Program
 // What would happen, is there would be a compiling error. The code couldnt run properly.
 // in most cases, it VS will tell you "; expected", as well as that is it is in CompileErrors.cs as well as the line and column number within the code where the error is detected. (8,49)
 // Sometimes, you will also be given error codes. Feel free to copy the error code and do a quick google on it!
+// A similar error will throw if you are missing a curly brace where one is expected. "} expected."
