197f1c1cfbeadf97ef9a44d6d1c25d15d4815e9f
Lacey Walker <bluelarksong@gmail.com>
2021-05-13 01:51:24 -0500

    CompileErrors notes

---
+++ CSharp/CompileErrors.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Displays "Hello World" on the terminal.
+        System.Console.Writeline("Hello Lacey!"); //<-- lets delete this semi colon from the code we made earlier in CSharpProgram.cs
+    }
+}
+
+// What would happen, is there would be a compiling error. The code couldnt run properly.
+// in most cases, it VS will tell you "; expected", as well as that is it is in CompileErrors.cs as well as the line and column number within the code where the error is detected. (8,49)
+// Sometimes, you will also be given error codes. Feel free to copy the error code and do a quick google on it!
