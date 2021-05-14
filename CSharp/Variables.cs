fe70315e2b428e678d89a2ed5dc4c21d66011641
Lacey Walker <bluelarksong@gmail.com>
2021-05-14 16:22:26 -0500

    Talking about variables

--- CSharp/Variables.cs
+++ CSharp/Variables.cs
@@ -25,3 +25,25 @@ class Program
 //hello                                                                                                           
 //6                                                                                                               
 //8
+
+// If you want to change the meaning of a variable, that's fine. but it can only be declared once. for example:
+class Program
+{
+    static void Main(string[] args)
+    {
+        var number = 4;
+        var greeting = "hello";
+        System.Console.WriteLine(number);
+        System.Console.WriteLine(greeting);
+        System.Console.WriteLine(number + 2);
+        number = 10; // <--
+        System.Console.WriteLine(12 - number);
+    }
+}
+
+// we can asign the variable a new meaning, but we only use var once.
+// You can only delcare one variable at a time with the same name n the same scope.
+// If you use var again for the same variable, the program will treat it as a different variable within the program, but with the same name.
+// This would give an error. "CS0219: The variable 'number' is assigned, but it's value is never used"
+// You would need to remove "var" so it can be treated as an assignment, not a declaration.
+// Remeber that you can only declare a variable once, but that you can assign it as many times as you need.
