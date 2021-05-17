b028faa8ea23b23a1b391d0434ca35008b6288d3
Lacey Walker <bluelarksong@gmail.com>
2021-05-16 19:54:09 -0500

    learning some shortcuts

--- CSharp/Types.cs
+++ CSharp/Types.cs
@@ -40,3 +40,90 @@ class Program2
         System.Console.WriteLine(number + 2);
     }
 }
+
+// You can also assign them directly, works the same.
+
+class Program3
+{
+    static void Main(string[] args)
+    {
+        System.Int32 number = 10;
+        System.String greeting = "What's up?";
+        System.Console.WriteLine(nunber);
+        System.Console.WriteLine(greeting);
+        System.Console.WriteLine(number + 2);
+    }
+}
+
+// you can also use a GetType here.
+class Program3
+{
+    static void Main(string[] args)
+    {
+        System.Int32 number = 10;
+        System.String greeting = "What's up?";
+        System.Console.WriteLine(nunber.GetType()); //<-----
+        System.Console.WriteLine(greeting.GetType()); //<-----
+        System.Console.WriteLine(number + 2);
+    }
+}
+
+// The console would return:
+// System.Int32
+// System.String
+// What's up?
+// 12
+
+// Here are some shortcuts!
+// int = System.Int32
+// string = System.String
+
+class Program3
+{
+    static void Main(string[] args)
+    {
+        int number = 10; //<----
+        string greeting = "What's up?"; //<---- See? This works great, too.
+        System.Console.WriteLine(nunber.GetType());
+        System.Console.WriteLine(greeting.GetType());
+        System.Console.WriteLine(number + 2);
+    }
+}
+
+// Here's a program that uses variable declarations with explocit types for the same integer, floating point number, Boolean value, string and character value that we saw earlier. 
+class Program4
+{
+    static void Main(string[] args)
+    {
+        System.Int32 wholeNumber = 4;
+        System.Double pi = 3.1415;
+        System.Boolean status = true;
+        System.String greeting = "Hello, world!";
+        System.Char oneLetter = 'z';
+
+        System.Console.WriteLine(wholeNumber.GetType());
+        System.Console.WriteLine(pi.GetType());
+        System.Console.WriteLine(status.GetType());
+        System.Console.WriteLine(greeting.GetType());
+        System.Console.WriteLine(oneLetter.GetType());
+    }
+}
+// Now, lets apply some shortcuts, since that is what's used usually anyway.
+
+class Program5
+{
+    static void Main(string[] args)
+    {
+        int wholeNumber = 4;
+        double pi = 3.1415;
+        bool status = true;
+        string greeting = "Hello, world!";
+        char oneLetter = 'z';
+
+        System.Console.WriteLine(wholeNumber.GetType());
+        System.Console.WriteLine(pi.GetType());
+        System.Console.WriteLine(status.GetType());
+        System.Console.WriteLine(greeting.GetType());
+        System.Console.WriteLine(oneLetter.GetType());
+    }
+}
