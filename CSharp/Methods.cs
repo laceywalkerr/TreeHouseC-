2e8b927174a43e8ab6d41a43dda5afb6b74b6e0e
Lacey Walker <bluelarksong@gmail.com>
2021-05-19 22:05:49 -0500

    System.Threading namespace and the Thread.Sleep method

--- CSharp/Methods.cs
+++ CSharp/Methods.cs
@@ -5,12 +5,15 @@
 // Discount for large orders
 
 using System;
+using System.Threading;
 
 class Program
 {
     static void Main(string[] args) //<---- This is our method
     {
-        Console.Writeline("Welcome to the catfood store!");
+        Console.WriteLine("Welcome to the catfood store!");
+        System.Threading.Thread.Sleep(3000); // <--- This method makes it wait for 3 seconds before loading anything else to the console.
+        Console.WriteLine("done sleeping.");
     }
 }
 
@@ -22,3 +25,8 @@ class Program
 
     }
 }
+
+// Notes:
+// A method is a group of code statements that are put together to perform a particular task
+// "Writeline" for example, is a method. The task it carries out is to write a line of text out to the console.
+// A method, like a function, is a set of instructions that perform a task. The difference is that a method is associated with an object, while a function is not. (Source: Codecademy.com)
