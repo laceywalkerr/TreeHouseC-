17c7d9a39ce4b81b8bdb04c6ac557bca1b73519c
Lacey Walker <bluelarksong@gmail.com>
2021-05-19 21:54:00 -0500

    completed item one of the requirements

---
+++ CSharp/Methods.cs
@@ -0,0 +1,24 @@
+//List of CatFood Store Features
+// X- Display welcome message -X
+// Ask for quality
+// Calculate Total
+// Discount for large orders
+
+using System;
+
+class Program
+{
+    static void Main(string[] args) //<---- This is our method
+    {
+        Console.Writeline("Welcome to the catfood store!");
+    }
+}
+
+//Code we started with:
+class Program
+{
+    static void Main(string[] args)
+    {
+
+    }
+}
