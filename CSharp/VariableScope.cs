576109fe75f49876884bf8ea86e6798a335d7b05
Lacey Walker <bluelarksong@gmail.com>
2021-05-19 22:41:41 -0500

    Made new file, VariableScope.cs

---
+++ CSharp/VariableScope.cs
@@ -0,0 +1,3 @@
+// If you declare a variable within a method, it's only accessible within that method. But trust us, that's a good thing! (- TeamTreeHouse)
+// Variable declared within a method is accessible only within that method
+// Can declare multiple variables with same name in different scopes
