d25f0f43ede4eee0f9d5adbd1450d5358010bf77
Lacey Walker <bluelarksong@gmail.com>
2021-05-26 20:28:19 -0500

    changed code to have two seperate arguments

--- CSharp/MethodArguments.cs
+++ CSharp/MethodArguments.cs
@@ -1,16 +1,37 @@
+// A parameter is a special variable that you declare at the start of a method.
+// It 's just like declaring a variable; you specify a type and a name for the parameter.
+// You can specify multiple parameters by separating them with commas.
+
+// Within the method body, you can access the parameter just like you would a variable.
+// In the Add method, we print the value of the first parameter, and then print the value of the second parameter.
+// Then we print the result of adding first and second together.
+
+// When a method takes parameters, you need to provide argument values when calling that method.
+// C# sets each parameter variable with the value in the argument.
+// https://teamtreehouse.com/library/method-arguments-5#notes
+
 using System;
 
 class Program
 {
-    static void Wait()
+    static void Add(double first, double second)
     {
-        Console.WriteLine("Waiting...");
-        Thread.Sleep(3000);
-        Console.WriteLine("Done");
+        Console.WriteLine(first);
+        Console.WriteLine(second);
+        Console.WriteLine(first + second);
+    }
+    // This would print out in order: the first parameter, the second parameter, and then lastly the result of adding the first and second parameter together.
+
+    static void Subtract(double first, double second)
+    {
+        Console.WriteLine(first);
+        Console.WriteLine(second);
+        Console.WriteLine(first - second);
     }
 
+    // When a method takes parameters, you need to provide argument values when calling that method.    
     static void Main(string[] args)
     {
-        Wait();
+        Add(3, 5);
     }
 }
