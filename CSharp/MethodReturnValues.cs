a5141d3cf02e07484d05f5cb725bb8fb0c8c7c18
Lacey Walker <bluelarksong@gmail.com>
2021-05-26 21:29:43 -0500

    saving progress

--- CSharp/MethodReturnValues.cs
+++ CSharp/MethodReturnValues.cs
@@ -6,11 +6,32 @@ class Program
     static void Ask(string question)
     {
         Console.WriteLine(question);
+        string answer = Console.ReadLine();
+        //Console.Readline will return the users responce when the question is prompted, and then gets assigned to the "answer" variable.
+        Console.WriteLine(answer);
+    }
+
+    static double Add(double first, double second)
+    {
+        return first + second;
+    }
+
+    static double Subtract(double first, double second)
+    {
+        return first - second;
     }
 
     static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the cat food store!");
         Ask("How many cans are you ordering?");
+
+        // Just gonna use a console writeline to throw a space between the kind of code I'm calling back, makes it easier to read.
+        Console.WriteLine("-----------------");
+
+        double total = Add(3, 5);
+        Console.WriteLine(total);
+        double remaining = Subtract(21.3, 7.1);
+        Console.WriteLine(remaining);
     }
 }
