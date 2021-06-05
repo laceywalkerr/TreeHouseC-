94ba93fc376537f5ed4332233df13ebf01d4595b
Lacey Walker <bluelarksong@gmail.com>
2021-06-05 04:15:09 -0500

    return vs print

--- CSharp/PrintingAndReturning.cs
+++ CSharp/PrintingAndReturning.cs
@@ -1,1 +1,6 @@
-return and print are statements, not functions
+Return and print are statements, not functions
+
+Print just shows the human user a string representing what is going on inside the computer.
+The computer cannot make use of that printing.
+Return is how a function gives back a value.
+This value is often unseen by the human user, but it can be used by the computer in further functions.
