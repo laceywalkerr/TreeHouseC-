259052c01411d88e97fee2fc50a0987a74628114
Lacey Walker <bluelarksong@gmail.com>
2021-06-05 04:17:37 -0500

    commented comments out, and some code so it wasn't as angry

--- CSharp/PrintingAndReturning.cs
+++ CSharp/PrintingAndReturning.cs
@@ -1,30 +1,30 @@
-Return and print are statements, not functions
+// Return and print are statements, not functions
 
-Print just shows the human user a string representing what is going on inside the computer.
-The computer cannot make use of that printing.
-Return is how a function gives back a value.
-This value is often unseen by the human user, but it can be used by the computer in further functions.
+// Print just shows the human user a string representing what is going on inside the computer.
+// The computer cannot make use of that printing.
+// Return is how a function gives back a value.
+// This value is often unseen by the human user, but it can be used by the computer in further functions.
 
-return is the main way that a function returns a value.All functions will
-return a value,
-    and
-if there is no
-return statement(or yield but don’ t worry about that yet), it will
-return None.
-The value that is returned by a function can then be further used as an argument passed to another function,
-stored as a variable, or just printed
-for the benefit of the human user.
+// return is the main way that a function returns a value.All functions will
+// return a value,
+//     and
+// if there is no
+// return statement(or yield but don’ t worry about that yet), it will
+// return None.
+// The value that is returned by a function can then be further used as an argument passed to another function,
+// stored as a variable, or just printed
+// for the benefit of the human user.
 
-Consider these two programs:
+// Consider these two programs:
 
-    def function_that_prints():
-    print "I printed"
+//     def function_that_prints():
+//     print "I printed"
 
-def function_that_returns():
-    return "I returned"
+// def function_that_returns():
+//     return "I returned"
 
-f1 = function_that_prints()
-f2 = function_that_returns()
-print "Now let us see what the values of f1 and f2 are"
-print f1
-print f2
+// f1 = function_that_prints()
+// f2 = function_that_returns()
+// print "Now let us see what the values of f1 and f2 are"
+// print f1
+// print f2
